1416be8f4108ad82084ba1175bc7c3196684f966
Ajaykumar Yadav <140816066+Ajay9330@users.noreply.github.com>
2024-11-08 04:50:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the Express server
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
